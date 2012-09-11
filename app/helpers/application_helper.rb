0ad83ac9900fa77cfc2f0f9bf05c90b6efeddc62
Tad Preston <tadpreston@gmail.com>
2012-09-10 20:36:34 -0500

    Clean up the authentication messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def flash_message_class(flash_type)
+    message_class = "message"
+    case flash_type.to_s
+    when 'error'
+      message_class += " red-gradient"
+    when 'notice'
+      message_class += " blue-gradient"
+    when 'alert'
+      message_class += " orange-gradient"
+    else
+      message_class += " blue-gradient"
+    end
+  end
 end
