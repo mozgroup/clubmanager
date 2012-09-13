7aefe51057b7fb2e0214318f7dcf3435b060488d
Tad Preston <tadpreston@gmail.com>
2012-09-13 12:30:02 -0500

    Customize the admin user index

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,28 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :full_name
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+
+  filter :last_name
+  filter :email
+  filter :employee_number
+  filter :title
+
+  form do |f|
+    f.inputs "Employee Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :employee_number
+      f.input :title
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.buttons
+  end
 end
