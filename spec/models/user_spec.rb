ee01ebb6cce9983e49f5c57baac5e20645e79de2
Tad Preston <tadpreston@gmail.com>
2012-09-13 13:16:24 -0500

    Annotate models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  failed_attempts        :integer          default(0)
+#  locked_at              :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  employee_number        :string(255)
+#  title                  :string(255)
+#  first_name             :string(255)
+#  last_name              :string(255)
+#
+
 require 'spec_helper'
 
 describe User do
