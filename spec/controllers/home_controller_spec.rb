4c913f8153409be82f9196309ce259c461aea1de
Tad Preston <tadpreston@gmail.com>
2012-09-14 14:33:59 -0500

    Clean up tests so that all pass

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,7 +5,7 @@ describe HomeController do
   describe "GET 'index'" do
     it "should be successful" do
       get 'index'
-      response.should be_success
+      response.should redirect_to(new_user_session_path)
     end
   end
 
