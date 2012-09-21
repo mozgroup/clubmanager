09b8730f5a611af14772c953d39646fe866424fb
Tad Preston <tadpreston@gmail.com>
2012-09-21 09:07:24 -0500

    Change the url to the dashboard in the shortcuts section

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -21,8 +21,8 @@ describe 'HomePage' do
     it { should have_selector('header', text: user.title) }
     it { should have_selector('li.current a.shortcut-dashboard') }
     it { should_not have_selector('li.current a.shortcut-messages') }
-    it { should have_link('Dashboard', href: './') }
-    it { should have_link('Messages', href: '/inbox') }
+    it { should have_link('Dashboard', href: root_url) }
+    it { should have_link('Messages', href: inbox_path) }
 
     it { should have_selector('section#menu') }
     it { should have_selector('span.count', text: user.envelopes.unread_count.to_s) }
