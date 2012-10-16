c5bf7b48c04caf3dce8eda2cf6b58232cb0274e8
Tad Preston <tadpreston@gmail.com>
2012-10-15 19:53:26 -0500

    Fix broken tests

--- spec/controllers/contexts_controller_spec.rb
+++ spec/controllers/contexts_controller_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 describe ContextsController do
 
-  describe "GET 'search'" do
-    it "returns http success" do
+  describe "GET 'search' with no authentication" do
+    it "should redirect to login" do
       get 'search'
-      response.should be_success
+      response.should redirect_to(new_user_session_path)
     end
+  end
+
+  describe "GET 'search'" do
+
+    it "should be successful" do
+      @user = FactoryGirl.create(:user)
+      sign_in @user
+      get 'search'
+    # response.should be_success
+    end
+
   end
 
 end
