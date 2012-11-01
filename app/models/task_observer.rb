b7d03ef1cd10d2df60aad93238201465ce396373
Tad Preston <tadpreston@gmail.com>
2012-10-31 22:03:07 -0500

    Cleaned up tests

--- app/models/task_observer.rb
+++ app/models/task_observer.rb
@@ -4,13 +4,25 @@ class TaskObserver < ActiveRecord::Observer
     Notifier::Tasks.deliver_assigned_task_message(task)
   end
 
+  def before_claim(task, transition)
+    task.claimed_at = Time.now.zone
+  end
+
   def after_claim(task, transition)
     task.log_task_claimed("Task #{task.name} has been claimed by #{task.assigned_to}", task.owner_full_name)
     Notifier::Tasks.deliver_claim_task_message(task)
   end
 
+  def before_start(task, transition)
+    task.started_at = Time.now.zone
+  end
+
   def after_start(task, transition)
     task.log_task_started("Task #{task.name} has been started by #{task.assigned_to}", task.owner_full_name)
+  end
+
+  def before_complete(task, transition)
+    task.completed_at = Time.now.zone
   end
 
   def after_complete(task, transition)
