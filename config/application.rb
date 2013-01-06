5df6870af1c25ddf45365d5abefeed3f897fd651
Tad Preston <tpreston@udsamerica.com>
2013-01-05 18:25:33 -0600

    Added line for heroku during asset compilation

--- config/application.rb
+++ config/application.rb
@@ -68,7 +68,7 @@ module ClubManager
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-#    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
