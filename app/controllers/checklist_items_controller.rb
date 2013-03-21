e263749ec8ddd9b7d223bdfeb1623e3f51396067
Tad Preston <tpreston@udsamerica.com>
2013-03-20 20:57:27 -0500

    Add edit functionality for checklist_item and rearranged the page

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -19,6 +19,20 @@ class ChecklistItemsController < ApplicationController
     @checklist_item = ChecklistItem.find params[:id]
   end
 
+  def edit
+    @checklist_item = ChecklistItem.find params[:id]
+  end
+
+  def update
+    @checklist_item = ChecklistItem.find params[:id]
+    if @checklist_item.update_attributes(params[:checklist_item])
+      flash[:notice] = 'Checklist item was successfully updated'
+      redirect_to @checklist_item
+    else
+      render 'edit'
+    end
+  end
+
   protected
 
   def complete_item
